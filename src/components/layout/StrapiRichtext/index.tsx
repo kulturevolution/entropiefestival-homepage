771883389c720d49aca003b1bbedb4f996c98420
volkram <volkram@volkramweber.de>
2024-10-04 22:24:39 +0200

    blocks renderer headings

--- src/components/layout/StrapiRichtext/index.tsx
+++ src/components/layout/StrapiRichtext/index.tsx
@@ -11,6 +11,26 @@ const StrapiRichtext: React.FC<{ content: BlocksContent }> = ({ content }) => {
         paragraph: ({ children }) => (
           <p className='mb-2 whitespace-pre-wrap lg:mb-4'>{children}</p>
         ),
+        heading: ({ children, level }) => {
+          switch (level) {
+            case 1:
+              return (
+                <h1 className='mb-6 text-[64px] font-black'>{children}</h1>
+              );
+            case 2:
+              return (
+                <h2 className='mb-4 text-[44px] font-black'>{children}</h2>
+              );
+            case 3:
+              return (
+                <h3 className='mb-2 text-[28px] font-black'>{children}</h3>
+              );
+            default:
+              return (
+                <h4 className='mb-1.5 text-[22px] font-black'>{children}</h4>
+              );
+          }
+        },
       }}
     />
   );
