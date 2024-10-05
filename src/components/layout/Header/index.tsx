0a9b2f8c685422e134a51f3411da4ac048f3d7f6
volkram <volkram@volkramweber.de>
2024-10-05 20:53:36 +0200

    hide logo on mobile homepage

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -8,9 +8,12 @@ import MenuOpen from '@/components/gfx/MenuOpen';
 import LogoSmall from '@/components/gfx/LogoSmall';
 import { useMainMenuStore } from '@/zustand/mainMenuStore';
 import LogoSmallHover from '@/components/gfx/LogoSmallHover';
+import { usePathname, useRouter } from 'next/navigation';
+import classNames from 'classnames';
 
 const Header: React.FC<{ locale: string }> = ({ locale }) => {
   const { menuOpen, setMenu } = useMainMenuStore();
+  const pathname = usePathname();
 
   useEffect(() => {
     setMenu(menuOpen);
@@ -24,7 +27,11 @@ const Header: React.FC<{ locale: string }> = ({ locale }) => {
   return (
     <div className='pointer-events-none absolute left-0 right-0 z-30 mx-5 my-6 flex items-center gap-x-2 lg:fixed lg:mx-16 lg:my-16'>
       <Link href={`/${locale}`} className='group pointer-events-auto'>
-        <LogoFull className='lg:hidden' />
+        <LogoFull
+          className={classNames('lg:hidden', {
+            'opacity-0': pathname === `/${locale}`,
+          })}
+        />
         <LogoSmall className='hidden h-[70px] lg:block group-hover:lg:hidden' />
         <LogoSmallHover className='hidden h-[70px] group-hover:lg:block' />
       </Link>
