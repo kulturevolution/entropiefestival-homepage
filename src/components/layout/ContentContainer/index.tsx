8f382055647ebd72da5d01650aa5896041b3a193
volkram <volkram@volkramweber.de>
2024-10-05 20:43:30 +0200

    header fix

--- src/components/layout/ContentContainer/index.tsx
+++ src/components/layout/ContentContainer/index.tsx
@@ -16,7 +16,7 @@ const ContentContainer: React.FC<{ children: any }> = ({ children }) => {
   return (
     <div
       className={classNames(
-        'relative z-40 mr-6 pt-32 transition-all lg:mx-60 xl:mx-80',
+        'relative mr-6 pt-32 transition-all lg:mx-60 xl:mx-80',
         menuOpen
           ? 'translate-x-[calc(-1*(var(--menu-width)+285px))] opacity-40 lg:translate-x-0 lg:opacity-100'
           : 'translate-x-0'
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -22,13 +22,16 @@ const Header: React.FC<{ locale: string }> = ({ locale }) => {
   }, [menuOpen, setMenu]);
 
   return (
-    <div className='absolute left-0 right-0 z-30 mx-5 my-6 flex items-center gap-x-2 lg:fixed lg:mx-16 lg:my-16'>
-      <Link href={`/${locale}`} className='group'>
+    <div className='pointer-events-none absolute left-0 right-0 z-30 mx-5 my-6 flex items-center gap-x-2 lg:fixed lg:mx-16 lg:my-16'>
+      <Link href={`/${locale}`} className='group pointer-events-auto'>
         <LogoFull className='lg:hidden' />
         <LogoSmall className='hidden h-[70px] lg:block group-hover:lg:hidden' />
         <LogoSmallHover className='hidden h-[70px] group-hover:lg:block' />
       </Link>
-      <button onClick={() => setMenu(!menuOpen)} className='ml-auto lg:hidden'>
+      <button
+        onClick={() => setMenu(!menuOpen)}
+        className='pointer-events-auto ml-auto lg:hidden'
+      >
         {menuOpen ? <MenuClose /> : <MenuOpen />}
       </button>
     </div>
