f643947c7927c56105a491ea50347ded5a97ed6a
volkram <volkram@volkramweber.de>
2024-11-09 10:24:03 +0100

    content box height fix

--- src/components/layout/ContentContainer/index.tsx
+++ src/components/layout/ContentContainer/index.tsx
@@ -16,7 +16,7 @@ const ContentContainer: React.FC<{ children: any }> = ({ children }) => {
   return (
     <div
       className={classNames(
-        'min-h-100dvh relative mr-6 flex flex-col pt-32 transition-all lg:mx-60 xl:mx-80',
+        'min-h-dvh relative mr-6 flex flex-col pt-32 transition-all lg:mx-60 xl:mx-80',
         menuOpen
           ? 'translate-x-[calc(-1*(var(--menu-width)+285px))] opacity-40 lg:translate-x-0 lg:opacity-100'
           : 'translate-x-0'
