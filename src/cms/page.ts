9881ede23580e673068d30d61f41338cbef932d0
manueleigen <mail@manueleigen.de>
2025-07-06 13:07:16 +0200

    Selbstverständnis als Seite angelegt

--- src/cms/page.ts
+++ src/cms/page.ts
@@ -18,3 +18,23 @@ export async function getPageData(
     id: result?.data?.[0]?.id,
   };
 }
+export async function getPageDataByTitle(
+    title: string,
+    locale: string
+): Promise<any> {
+  const response = await fetch(
+      `${process.env.NEXT_CMS_API}/pages?filters[title][$eq]=${title}&locale=${locale}`,
+      {
+        headers: {
+          Authorization: `bearer ${process.env.NEXT_CMS_TOKEN}`,
+        },
+      }
+  );
+
+  const result = await response.json();
+  //console.log(JSON.stringify(result, null, 2));
+  return {
+    ...result?.data?.[0]?.attributes,
+    id: result?.data?.[0]?.id,
+  };
+}
